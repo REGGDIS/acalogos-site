4747261134df91b9bba74f0527453b2590eda62d
REGGDIS <regdis@gmail.com>
2025-01-24 10:51:26 -0300

    Eliminar archivo redundante index.html de la carpeta public

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     "./index.html", // Ruta al index.html dentro de la carpeta frontend
     "./src/**/*.{js,ts,jsx,tsx}", // Ruta a los archivos dentro de src
-    "./public/index.html",
   ],
   theme: {
     extend: {
